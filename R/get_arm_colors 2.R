2e6254373b3a5cc50401bbd71fe9f2311137e76a
Laura Symul <laurasymul@gmail.com>
2026-01-16 12:22:38 +0100

    remove duplicated files

--- R/get_arm_colors 2.R
+++
@@ -1,5 +0,0 @@
-
-get_arm_colors <- function(){
-  # c("dodgerblue2", "indianred1")
-  c("LACTIN-V" = "orange", "Placebo" = "gray", "Lactin-V" = "orange")
-}
--- R/get_cytokine_angle_colors 2.R
+++
@@ -1,5 +0,0 @@
-
-get_cytokine_angle_colors <- function(){
-  c("orange", "plum" , "mediumslateblue", "dodgerblue2", 
-    "turquoise3", "mediumaquamarine" , "olivedrab3", "gold", "orange")
-}
